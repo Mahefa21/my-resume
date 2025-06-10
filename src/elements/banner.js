9d14bd236dde6f4fe28d033ff0950057ed88bf65
Charly RAOELIMAHEFA <raoelimahefacharly@gmail.com>
2025-06-10 10:16:19 +0400

    Do not print but download my CV

--- src/elements/banner.js
+++ src/elements/banner.js
@@ -1,11 +1,12 @@
 import cDown from './../../src/assets/images/illustrations/chevron-down.png'
+import charlyCvPdf from './../../src/assets/documents/CV-RAOELIMAHEFA-Charly.pdf'
 
 document.querySelector('#banner').innerHTML = `
     <div class="container text-center text-lg-start">
         <h4>Bonjour, je suis<h4>
         <h4 class="display-4">RAOELIMAHEFA Charly</h4>
         <h5>Développeur web Full Stack Ruby/PHP</h5>
-        <a class="btn btn-outline-dark p-2 my-2 print-cv"> Telecharger mon CV</a>
+        <a href="${charlyCvPdf}" class="btn btn-outline-dark p-2 my-2"> Telecharger mon CV</a>
         <a class="btn btn-outline-dark p-2 ms-2 my-2" href="mailto:raoelimahefacharly@gmail.com" > Intéressé(e) par mon profil ?</a>
     </div>
     <a href="#about" class="c-btn">
